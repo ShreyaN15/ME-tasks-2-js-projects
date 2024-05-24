3234965b184d56fdcccdceec22d17572fe990181
ShreyaN15 <119808754+ShreyaN15@users.noreply.github.com>
2024-05-24 15:36:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,19 @@ function randcolour(){
   const color= `rgb(${red},${green},${blue})`;
   body.style.backgroundColor=color;
 }
+const input=document.getElementById("input");
+function reverser(str){
+  return str.split("").reverse().join("")
+  
+}
+function check(){
+  const value=input.value
+  const reverse=reverser(value)
+  if (value===reverse){
+    alert("palindrome")
+  }else{
+    alert("not palindrome")
+  }
+  
+  input.value=""
+}
